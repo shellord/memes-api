a25eb5f9fdf4f8e0a184ff46e5854f91e656d38f
saheenshoukath <saheenshoukath@gmail.com>
2021-01-10 18:00:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,10 +16,15 @@ app.get("/api", function (req, res) {
 })
 
 app.get("/api/memes/:subreddit", function (req, res) {
-    meme(req.params.subreddit, function (err, data) {
+    var times = 10;
+    memes=[]
+    for(var i=0; i < times; i++){
+         meme(req.params.subreddit, function (err, data) {
         if (err) return res.send(JSON.stringify({ "status": 200, "error": true, "response": err }))
-        return res.send(JSON.stringify({ "status": 200, "error": null, "response": data }))
-    })
+           memes.push(data)
+         })
+    }
+    return res.send(JSON.stringify({ "status": 200, "error": null, "response": memes }))
 })
 
 app.listen(process.env.PORT || 3000, () => {
